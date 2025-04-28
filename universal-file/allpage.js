d5b0fdf8caea431257fd91fade06f919de1e0a7d
yeh0315 <91412327+yeh0315@users.noreply.github.com>
2025-04-28 09:38:23 +0000

    Changes to be committed: 	modified:   universal-file/allpage.js 	modified:   universal-file/nav.html 	modified:   web-core/about-folder/about.html 	modified:   web-core/contact-folder/contact.html 	modified:   web-core/index-folder/index.html 	modified:   web-core/kiosk-folder/kiosk.html 	modified:   web-core/member-folder/member.html 	modified:   web-core/news-folder/news.html 	modified:   web-core/policy-folder/policy.html 	modified:   web-core/products-folder/products.html 	modified:   web-core/stores-folder/stores.html

--- universal-file/allpage.js
+++ universal-file/allpage.js
@@ -1,12 +1,12 @@
 // 自動載入 navbar 和 footer
 document.addEventListener("DOMContentLoaded", function() {
-  fetch('nav.html')
+  fetch('../../universal-file/nav.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('navbar').innerHTML = data;
       });
 
-  fetch('footer.html')
+  fetch('../../universal-file/footer.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('footer').innerHTML = data;
