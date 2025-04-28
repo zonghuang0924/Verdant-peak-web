ab146ab43bc73c40360209c74b823060feb1138d
yeh0315 <91412327+yeh0315@users.noreply.github.com>
2025-04-28 09:21:28 +0000

    Changes to be committed: 	deleted:    navbar.html 	modified:   universal-file/allpage.js 	new file:   universal-file/nav.html 	modified:   web-core/about-folder/about.html

--- universal-file/allpage.js
+++ universal-file/allpage.js
@@ -1,4 +1,14 @@
-$(document).ready(function(){
-  $('header').load('header.html');
-  $('footer').load('footer.html');
+// 自動載入 navbar 和 footer
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('nav.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('navbar').innerHTML = data;
+      });
+
+  fetch('footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('footer').innerHTML = data;
+      });
 });
