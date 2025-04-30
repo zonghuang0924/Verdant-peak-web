6f8cc35ac161c27baefb521f869ec23ea53b76af
verdant87 <verdantpeakdew@gmail.com>
2025-04-30 10:28:17 +0000

    Changes to be committed: 	modified:   universal-file/footer.html 	modified:   web-core/about-folder/about.html 	modified:   web-core/index-folder/index.html 	modified:   web-core/policy-folder/policy.html 	modified:   web-core/products-folder/products.html 	modified:   web-core/products-folder/products.js

--- web-core/products-folder/products.js
+++ web-core/products-folder/products.js
@@ -2,36 +2,28 @@
 const products = [
   {
     id: "original-drink",
-    name: "原味植萃飲",
+    name: "翠峰甘露大道至簡",
     category: "classic",
-    description: "成分：天然草本精華、純水<br>保存：冷藏｜規格：350ml",
+    description: "成分：天然屏東九如檸檬、純水、赤藻糖醇<br>保存：冷藏｜規格：350ml",
     price: 65,
     image: "images/original1.jpg"
   },
   {
     id: "rose-tea",
-    name: "玫瑰花茶",
+    name: "翠峰甘露薔薇花火",
     category: "rose",
-    description: "成分：玫瑰、蜂蜜、純水<br>保存：冷藏｜規格：350ml",
+    description: "成分：玫瑰、九如檸檬、純水、赤藻糖醇<br>保存：冷藏｜規格：350ml",
     price: 75,
     image: "images/rose1.jpg"
   },
   {
     id: "blue-drink",
-    name: "藍玫瑰沁涼飲",
+    name: "翠峰甘露蔚藍狂潮",
     category: "blue",
-    description: "成分：藍玫瑰萃取、檸檬<br>保存：冷藏｜規格：350ml",
+    description: "成分：藍色螺旋藻、九如檸檬、純水、赤藻糖醇<br>保存：冷藏｜規格：350ml",
     price: 85,
     image: "images/blue1.jpg"
-  },
-  {
-    id: "summer-drink",
-    name: "夏日限定水果飲",
-    category: "limited",
-    description: "成分：綜合水果、薄荷氣泡水<br>保存：冷藏｜規格：500ml",
-    price: 95,
-    image: "images/limited1.jpg"
-  }
+ }
 ];
 
 // 獲取商品展示區域
