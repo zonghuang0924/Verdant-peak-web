fcc117ce9c387c22618a9284926cc8e3c3550ddf
verdant87 <verdantpeakdew@gmail.com>
2025-05-03 17:10:04 +0000

    Changes to be committed: 	modified:   universal-file/allpage.js

--- universal-file/allpage.js
+++ universal-file/allpage.js
@@ -1,12 +1,12 @@
 // 自動載入 navbar 和 footer
 document.addEventListener("DOMContentLoaded", function() {
-  fetch('universal-file/nav.html')
+  fetch('nav.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('navbar').innerHTML = data;
       });
 
-  fetch('universal-file/footer.html')
+  fetch('footer.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('footer').innerHTML = data;
