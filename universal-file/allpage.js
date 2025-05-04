928a954a39a0137a2b047d0bc8ebfa2f1d578cdd
yeh0315 <91412327+yeh0315@users.noreply.github.com>
2025-05-04 16:18:19 +0000

    Changes to be committed: 	modified:   universal-file/allpage.js

--- universal-file/allpage.js
+++ universal-file/allpage.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('/universal-file/nav.html')
+    fetch('../../universal-file/nav.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('navbar').innerHTML = data;
       })
       .catch(err => console.error('Navbar loading failed:', err));
   
-    fetch('/universal-file/footer.html')
+    fetch('../../universal-file/footer.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('footer').innerHTML = data;
